6c16501d2018eae98ef3ac1d4a29b867593ce066
Sergiy Poddubka <poseal@gmail.com>
2024-04-16 12:36:51 +0300

    add model Contact

---
+++ app/src/main/java/com/example/contacts/model/Contact.kt
@@ -0,0 +1,11 @@
+package com.example.contacts.model
+
+data class Contact (
+    val imageId: Int = -1,
+    val email: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val password: String = "",
+    val profession: String = "",
+    val mobile: String = ""
+)
