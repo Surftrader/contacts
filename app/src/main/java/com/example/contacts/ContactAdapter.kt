dc0a33c5e9ea6569ad1f1377e2dc8ecf36797833
Sergiy Poddubka <poseal@gmail.com>
2024-04-20 21:19:24 +0300

    use ListAdapter instead of RecyclerView.Adapter

--- app/src/main/java/com/example/contacts/ContactAdapter.kt
+++ app/src/main/java/com/example/contacts/ContactAdapter.kt
@@ -3,16 +3,43 @@ package com.example.contacts
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contacts.data.DataSource
 import com.example.contacts.data.InternDataSource
 import com.example.contacts.databinding.ContactItemBinding
 import com.example.contacts.model.Contact
 
-class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactHolder>() {
+class ContactAdapter : ListAdapter<Contact, ContactAdapter.ContactHolder>(ContactDiffCallBack()) {
 
     private val dataSource: DataSource<Contact> = InternDataSource()
     private val contactList: List<Contact> = dataSource.getContacts()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactHolder {
+        return ContactHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.contact_item, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return contactList.size
+    }
+
+    override fun onBindViewHolder(holder: ContactHolder, position: Int) {
+        holder.bind(contactList[position])
+    }
+
+    private class ContactDiffCallBack : DiffUtil.ItemCallback<Contact>() {
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact):
+                Boolean = oldItem.email == newItem.email
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact):
+                Boolean = oldItem == newItem
+    }
 
     class ContactHolder(item: View) : RecyclerView.ViewHolder(item) {
         private val binding = ContactItemBinding.bind(item)
@@ -25,19 +52,5 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactHolder>() {
                 append(contact.lastName)
             }
         }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactHolder {
-        return ContactHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
-        )
-    }
-
-    override fun getItemCount(): Int {
-        return contactList.size
-    }
-
-    override fun onBindViewHolder(holder: ContactHolder, position: Int) {
-        holder.bind(contactList[position])
     }
 }
