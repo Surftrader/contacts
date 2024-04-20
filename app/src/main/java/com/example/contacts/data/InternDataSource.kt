b74f4de07861979d153e4aa3521783b5c85c19fc
Sergiy Poddubka <poseal@gmail.com>
2024-04-20 21:48:09 +0300

    change contacts content

--- app/src/main/java/com/example/contacts/data/InternDataSource.kt
+++ app/src/main/java/com/example/contacts/data/InternDataSource.kt
@@ -5,16 +5,42 @@ import com.example.contacts.model.Contact
 
 class InternDataSource : DataSource<Contact> {
     override fun getContacts(): List<Contact> {
-        return List(10) {
+        return listOf(
             Contact(
                 imageId = R.drawable.ava_smith,
-                email = "$it",
-                firstName = "Ava $it",
-                lastName = "Smith $it",
-                password = "$it",
-                profession = "Photograph $it",
-                mobile = "$it"
+                email = "ava.smith@gmail.com",
+                firstName = "Ava",
+                lastName = "Smith",
+                profession = "Photograph"
+            ),
+            Contact(
+                imageId = R.drawable.jessie_brown,
+                email = "jessie.brown@gmail.com",
+                firstName = "Jessie",
+                lastName = "Brown",
+                profession = "Actress"
+            ),
+            Contact(
+                imageId = R.drawable.jackie_taylor,
+                email = "jackie.taylor@gmail.com",
+                firstName = "Jackie",
+                lastName = "Taylor",
+                profession = "Financier"
+            ),
+            Contact(
+                imageId = R.drawable.jenny_walker,
+                email = "jenny.walker@gmail.com",
+                firstName = "Jenny",
+                lastName = "Walker",
+                profession = "Make-up artist"
+            ),
+            Contact(
+                imageId = R.drawable.freddy_harris,
+                email = "freddy.harris@gmail.com",
+                firstName = "Freddy",
+                lastName = "Harris",
+                profession = "Secretary"
             )
-        }
+        )
     }
 }
