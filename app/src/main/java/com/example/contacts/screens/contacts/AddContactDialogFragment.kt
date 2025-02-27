52c278a7ac84ab86c51458de17769c961c7ca14b
Sergiy Poddubka <poseal@gmail.com>
2025-02-27 18:13:43 +0200

    add extra function in Context for Toast

--- app/src/main/java/com/example/contacts/screens/contacts/AddContactDialogFragment.kt
+++ app/src/main/java/com/example/contacts/screens/contacts/AddContactDialogFragment.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.contacts.R
 import com.example.contacts.databinding.DialogAddContactBinding
 import com.example.contacts.model.Contact
+import com.example.contacts.util.showToast
 
 class AddContactDialogFragment : DialogFragment() {
 
@@ -70,7 +70,7 @@ class AddContactDialogFragment : DialogFragment() {
                 listener?.onContactAdded(newContact)
                 dismiss()
             } else {
-                Toast.makeText(requireContext(), R.string.required_fields, Toast.LENGTH_SHORT).show()
+                requireContext().showToast(getString(R.string.required_fields))
             }
         }
 
